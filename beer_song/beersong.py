b27c8f6f7c9a8b09715c5a5c72857b5ab6c278c1
Vadim <bardiervadim97@gmail.com>
2019-05-03 19:34:45 +0300

    1 commit

--- beer_song/beersong.py
+++ beer_song/beersong.py
@@ -1,16 +1,40 @@
+################  ENG VERSION  ##################
+# word = 'bottles'
+# for beer_num in range(99, 0, -1):
+#     print(beer_num, word, "of beer on the wall.")
+#     print(beer_num, word, "of beer.")
+#     print("Take one down.")
+#     print("Pass it around.")
+#     if beer_num == 1:
+#         print("No more bottles on the wall.")
+#     else:
+#         new_num = beer_num - 1
+#         if new_num == 1:
+#             word = 'bottle'
+#         print(new_num, word, "of beer on the wall")
+#         print()
+
+################  RUS VERSION  ##################
+
 word = 'bottles'
 for beer_num in range(99, 0, -1):
-    print(beer_num, word, "of beer on the wall.")
-    print(beer_num, word, "of beer.")
-    print("Take one down.")
-    print("Pass it around.")
+    print(beer_num, word, "пива на стене.")
+    print(beer_num, word, "пива.")
+    print("Возьми одну.")
+    print("Выпей до дна.")
     if beer_num == 1:
-        print("No more bottles on the wall.")
+        print("Нет больше бутылок на стене.")
     else:
         new_num = beer_num - 1
-        if new_num == 1:
-            word = 'bottle'
-            print(new_num, word, "of beer on the wall")
+        if new_num >= 11 and new_num <= 19:
+            word = 'бутылок'
+            print(new_num, word, "пива на стене.")
         else:
-            print(new_num, word, "of beer on the wall")
-        print()
+            if new_num % 10 == 1:
+                word = 'бутылка'
+            elif new_num % 10 in (2, 3, 4):
+                word = 'бутылки'
+            else:
+                word = 'бутылок'
+        print(new_num, word, "пива на стене")
+    print()
