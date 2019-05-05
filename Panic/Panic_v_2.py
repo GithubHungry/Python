78acb6a854e28fa767383b1d8d91c5c475e6eca1
Vadim <bardiervadim97@gmail.com>
2019-05-05 17:26:56 +0300

    1 commit

---
+++ Panic/Panic_v_2.py
@@ -0,0 +1,12 @@
+phase = "Don`t panic!"
+plist = list(phase)
+
+print(phase)
+print(plist)
+
+new_phase = ''.join(plist[1:3:1])  # Выделяет on из plist
+new_phase = new_phase + ''.join([plist[5], plist[4], plist[7], plist[6]])  # К on присоединяются еще ' ','t','a','p'
+#  Параллельтно все преобразовывается в строку
+
+print(plist)
+print(new_phase)  # Вывод преобразованного в строку массива
