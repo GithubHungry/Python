4085a506b2542c8c200facbe596939330216d8b9
Vadim <bardiervadim97@gmail.com>
2019-05-05 15:56:28 +0300

    1 commit

---
+++ Panic/Panic.py
@@ -0,0 +1,32 @@
+phase = "Don`t panic!"
+plist = list(phase)
+print(phase)
+print(plist)
+
+########## Мой вариант ##########
+
+# spis = ['D', '`', ' ', 'i', '!']  # Вспомогательный список для удаления
+# for letter in plist:  # роход по списку с удалением совпадений
+#     if letter in spis:
+#         plist.remove(letter)
+# plist.pop(5)  # Удаляется вторая буква n
+# plist.pop()  # Удаляется буква c последняя  --?
+# plist.append('p')  # Добавляется в конец буква р
+# plist.remove('p')  # Удаляется буква р первая --?
+# plist.insert(2, ' ')  # Добавляется пробел после on
+
+
+########## Кинжный вариант ##########
+
+for i in range(4):
+    plist.pop()  # Удаляет с КОНЦА
+plist.pop(0)  # Удаляет первый символ в списке
+plist.remove('`')
+# plist.insert(3,plist.pop(2))
+# plist.insert(6,plist.pop(4))
+plist.extend([plist.pop(), plist.pop()])  # Сперва извлекает 2 последние буквы, затем вставляет их в конец ([] - важно)
+plist.insert(2, plist.pop(3))  # Извлекает пробел, а затем его вставляет
+
+new_phase = ''.join(plist)  # Преобразует массив в строку
+print(plist)
+print(new_phase)  # Вывод преобразованного в строку массива
