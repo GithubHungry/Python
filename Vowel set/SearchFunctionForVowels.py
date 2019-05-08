4334e34c379bc4e83f547e501977c8ea0d4741f3
Vadim <bardiervadim97@gmail.com>
2019-05-08 18:07:31 +0300

    1 commit

--- Vowel set/SearchFunctionForVowels.py
+++ Vowel set/SearchFunctionForVowels.py
@@ -1,9 +1,19 @@
-def search_for_vowels(word):
-    """Выводит найденные гласные в переданной строке"""
-    vowels = set('a,o,e,i,u,y')
-    found = vowels.intersection(set(word))
-    founded = list(found)
-    return founded
+def search_for_letters(letters: set, phase: str) -> set:
+    """Возвращает найденные буквы множеством в переданной строке"""
+    # vowels = set('aoeiuy')
+    # print(type(vowels))
+    # found = vowels.intersection(set(word))
+    # founded = list(found)
+    # return found
+    # return bool(founded)
+    return letters.intersection(set(phase))
 
 
-print(sorted(search_for_vowels(str(input('Введите строку для анализа:')))))
+def search_for_letters_v_2(phase: str, letters: str = 'aoiuye') -> set:
+    return set(letters).intersection(set(phase))
+
+
+setter = set('aoiue')
+print(search_for_letters(setter, str(input('Введите строку для анализа:'))))
+print(search_for_letters_v_2(str(input('Введите строку для анализа:')), str(input("Введите то,что мы будем искать"))))
+print(search_for_letters_v_2(str(input("Введи:"))))
