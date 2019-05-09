e1b0b879455de71ce1d88e2d22b222f521b95d7f
Vadim <bardiervadim97@gmail.com>
2019-05-09 18:21:14 +0300

    1 commit

--- Web/hello-flask.py
+++ Web/hello-flask.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request,redirect
 import SearchFunctionForVowels
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask'
+def hello() -> '302':
+    return redirect('/entry')  # Сообщает браузеру, что необходимо запросить альтернативный URL
 
 
-@app.route('/search4')
-def do_search() -> str:
-    return str(SearchFunctionForVowels.search_for_letters_v_2('life, the universe, and everything!', 'eiru,!'))
+@app.route('/search4', methods=['POST'])  # Метод POST как и в форме
+def do_search() -> 'html':
+    phrase = request.form['phrase']  # Получаем данные из формы
+    letters = request.form['letters']
+    result = str(SearchFunctionForVowels.search_for_letters_v_2(phrase, letters))
+    return render_template('results.html', the_letters=letters, the_phrase=phrase, the_title='Here are your results!',
+                           the_results=result)
 
 
-app.run()
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+app.run(debug=True)
