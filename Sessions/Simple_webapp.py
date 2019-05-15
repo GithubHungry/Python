584f79e4f26009c4b9fd18b05dd1879c2176f201
Vadim <bardiervadim97@gmail.com>
2019-05-15 17:46:42 +0300

    1 commit

--- Sessions/Simple_webapp.py
+++ Sessions/Simple_webapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 
 app = Flask(__name__)
+app.secret_key = '322228'
 
 
 @app.route('/')
@@ -9,10 +10,20 @@ def hello() -> str:
     return 'Hello from the simple webapp.'
 
 
+@app.route('/login')
+def do_login() -> str:
+    """login in system"""
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
 @app.route('/page1')
 def page1() -> str:
     """Page one"""
-    return 'This is page 1'
+    if 'logged_in' in session:
+        return 'This is page 1'
+    else:
+        return 'Oppps? problemez!'
 
 
 @app.route('/page2')
@@ -25,6 +36,22 @@ def page2() -> str:
 def page3() -> str:
     """Page three"""
     return 'This is page 3'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    """logout from system"""
+    session.pop('logged_in')  # also we can`t to change value to false == error
+    return 'You are now logged out from system'
+
+
+@app.route('/status')
+def show_status() -> str:
+    """Show user status"""
+    if 'logged_in' in session:  # there we can`t to check value true or false to identify ==error
+        return 'You are currently logged in'
+    else:
+        return 'You are NOT logged IN system'
 
 
 if __name__ == '__main__':
