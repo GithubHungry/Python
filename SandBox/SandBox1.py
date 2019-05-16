c5626dd078d1fd6a0053d15d22e286c13237f666
Vadim <bardiervadim97@gmail.com>
2019-05-16 23:01:16 +0300

    1 commit

--- SandBox/SandBox1.py
+++ SandBox/SandBox1.py
@@ -261,5 +261,18 @@ try:
     with open('myfile.txt') as fh:
         file_data = fh.read()
         print(file_data)
-except FileNotFoundError:
-    print('The data file is missing!')
+# except FileNotFoundError:  # Обработка определенной ошибки
+#     print('The data file is missing!')
+except PermissionError:
+    print('Error?')
+# except:  # Обработка любых исключений
+#     print('Unknown error')
+except Exception as err:
+    print('Some other error occurred:', str(err))
+
+# try:
+#     1/0
+# except:
+#     err=sys.exc_info()
+#     for e in err:
+#         print(e)
