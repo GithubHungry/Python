75b7f22c1c3281002b2e0fdf262d61016ac67f50
Vadim <bardiervadim97@gmail.com>
2019-08-09 10:44:44 +0300

    1 commit

--- projects/alien_invasion/game_functions.py
+++ projects/alien_invasion/game_functions.py
@@ -2,21 +2,31 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Check keydown events."""
+    if event.key == pygame.K_RIGHT:  # If we press right arrow
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:  # If we press left arrow
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Check keyup events."""
+    if event.key == pygame.K_RIGHT:  # If we up right arrow
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:  # If we up left arrow
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Checkin events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:  # If we press key on keyboard
-            if event.key == pygame.K_RIGHT:  # If we press right arrow
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:  # If we press left arrow
-                ship.moving_left = True
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
