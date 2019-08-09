3d958b2f8d34e70f611117ae18ef7e6e745fb9d2
Vadim <bardiervadim97@gmail.com>
2019-08-09 15:56:56 +0300

    1 commit

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from projects.alien_invasion.settings import Settings
 from projects.alien_invasion.ship import Ship
 from projects.alien_invasion import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game() -> None:
@@ -11,10 +12,12 @@ def run_game() -> None:
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))  # Create screen area
     pygame.display.set_caption("Alien Invasion")  # Create window title
     ship = Ship(screen, ai_settings)  # create ship
+    bullets = Group()  # Create group to save bullets
     while True:  # Game cycle
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship,bullets)
 
 
 run_game()
--- projects/alien_invasion/game_functions.py
+++ projects/alien_invasion/game_functions.py
@@ -1,13 +1,17 @@
 import sys
 import pygame
+from projects.alien_invasion.bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Check keydown events."""
     if event.key == pygame.K_RIGHT:  # If we press right arrow
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:  # If we press left arrow
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -18,19 +22,21 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Checkin events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and show new screen."""
     screen.fill(ai_settings.bg_color)  # Filling game area, atr tuple or var only
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()  # draw new(old) ship, after filling, because we wanna ship on background
     pygame.display.flip()  # This line update screen after each event
