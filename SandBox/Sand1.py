abe02369b198736a674b32f4d0dbd6bc6272794a
Vadim <bardiervadim97@gmail.com>
2019-10-26 20:30:01 +0300

    init

--- SandBox/Sand1.py
+++ SandBox/Sand1.py
@@ -1,3 +1,6 @@
+# from threading import Thread, Lock
+# from concurrent.futures import ThreadPoolExecutor
+
 # to_test = [
 #     ('1.0.0', '2.0.0'),
 #     ('1.0.0', '1.42.0'),
@@ -35,3 +38,62 @@
 #
 # if __name__ == "__main__":
 #     main()
+
+# class Noop:
+#     def __new__(cls, *args, **kwargs):
+#         print("Creating instance with {} and {}".format(args, kwargs))
+#         instance = super().__new__(cls)
+#         return instance
+#
+#     def __init__(self, *args, **kwargs):
+#         print("Initializing instance with {} and {}".format(args, kwargs))
+#
+#
+# noop = Noop(42, attr='value')
+
+
+# a = 0
+# mutex = Lock()
+#
+#
+# def function(arg):
+#     global a
+#     for _ in range(arg):
+#         mutex.acquire()
+#         a += 1
+#         mutex.release()
+#
+#
+# def main():
+#     threads = []
+#
+#     for i in range(5):
+#         thread = Thread(target=function, args=(10,))
+#         thread.start()
+#         threads.append(thread)
+#
+#     [t.join() for t in threads]
+#     print("----------------------", a)  # ???
+#
+#
+# main()
+
+# a = 0
+# mutex = Lock()
+#
+#
+# def function(arg):
+#     global a
+#     for _ in range(arg):
+#         mutex.acquire()
+#         a += 1
+#         mutex.release()
+#
+#
+# def main():
+#     with ThreadPoolExecutor(5) as executor:
+#         executor.map(function, [100000, 100000, 100000, 100000, 100000])
+#     print("----------------------", a)  # ???
+#
+#
+# main()
